2b1ae6ee590b9a807a0d18d8a55ab2a1eceabfca
MrInal-M3hta <mrinalmehta11@gmail.com>
2026-06-05 21:35:52 +0530

    Adding more ways to upload Image and Video in multer

--- LearningWithProject/src/middleware/multer.middleware.js
+++ LearningWithProject/src/middleware/multer.middleware.js
@@ -19,3 +19,59 @@ const storage = multer.diskStorage({
 export const upload = multer({ 
     storage, 
 }); // This creates a middleware called upload. Middleware means it runs before your controller.
+
+/*
+
+import multer from "multer";
+import fs from "fs/promises";
+
+const uploadPath = "./public/temp";
+
+// ensure folder exists
+async function ensureDir() {
+  try {
+    await fs.access(uploadPath);
+  } catch {
+    await fs.mkdir(uploadPath, { recursive: true });
+  }
+}
+await ensureDir();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath); // This means -> Save uploaded file inside { /public/temp }
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+export const uploadImage = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024 * 20,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images allowed"), false);
+    }
+  },
+});
+
+export const uploadVideo = multer({
+  storage,
+  limits: {
+    fileSize: 500 * 1024 * 1024, // 500 MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("video/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only videos allowed"));
+    }
+  },
+});
+
+*/
